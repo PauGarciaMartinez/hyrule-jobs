f49d2f24f6cddf7eae97e4e5a502663978385fa9
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-18 13:27:29 +0200

    Define Job array and fill it with data

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job';
 
 export default defineComponent({
   name: 'App',
   components: {},
   setup() {
-    const name = ref<string>('Link')
-    const age = ref<number | string>(25)
+    const jobs = ref<Job[]>([
+      { title: 'Farm worker', location: 'Lon-Lon Ranch', salary: 30000, id: '1' }, 
+      { title: 'Quarryman', location: 'Death Mountain', salary: 40000, id: '2' },
+      { title: 'Flute player', location: 'The Lost Woods', salary: 35000, id: '3' },
+      { title: 'Fisherman', location: 'Lake Hylia', salary: 21000, id: '4' },
+      { title: 'Prison guard', location: 'Gerudo Valley', salary: 32000, id: '5' }
+    ])
 
-    return { name, age }
+    return { jobs }
   }
 });
 </script>
